22bc1a5b698a3f1ef0c091689d4fd1385ea8bfb8
Elisson Alves Silva <nossilesilva@hotmail.com>
2018-08-09 20:51:56 -0400

    attached endpoints

--- app/routes/item.route.js
+++ app/routes/item.route.js
@@ -4,5 +4,14 @@ module.exports = app => {
 
     const controller = require('./../controllers/item.controller');
 
+    app.route('/v1/endpoints')
+        .post(controller.creteAll);
+
+    app.route('/v1/items')
+        .get(controller.getAll);
+
+    app.route('/v1/items/:businessId')
+        .get(controller.getById);
+
 
 };
